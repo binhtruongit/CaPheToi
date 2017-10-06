de9c5855f598c47029d52905bea3d29c6ae742b5
binhtruongit <32286232+binhtruongit@users.noreply.github.com>
2017-10-06 17:05:32 +0700

    Initial commit

---
+++ CaPheToi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CaPheToi.MvcApplication" Language="C#" %>
---
+++ CaPheToi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CaPheToi.Startup))]
+namespace CaPheToi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
